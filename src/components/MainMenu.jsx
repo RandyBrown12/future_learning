c28ca1f84ca474e3cfd8596cb0b3b08723680efa
XixWISExiX <addalooptoit@gmail.com>
2024-02-10 20:38:52 -0600

    Reactive Style changes for the Main Menu

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
+import Nav from "../Nav";
 
 const MainMenu = () => {
-    return ( 
-        <div className="center">
-            <h1 className="space_font white_text"> Learning Project </h1>
-            <Link to="/game"> 
-                <button className="button space_font terminal_style"> Start Game </button>
-            </Link>
+  return (
+    <div>
+      <div className="space_font">
+        <Nav />
+      </div>
+      <div className="center">
+        <h1 className="space_font white_text"> Learning Project </h1>
+        <div className="first-button">
+          <Link to="/game">
+            <button className="button space_font terminal_style">
+              {" "}
+              Start Game{" "}
+            </button>
+          </Link>
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default MainMenu;
