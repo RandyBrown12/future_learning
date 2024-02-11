86927d48e0321cc4e053d0c3366c530e10418695
XixWISExiX <addalooptoit@gmail.com>
2024-02-10 22:11:17 -0600

    Game.jsx changes added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,7 @@
 import "./Game.css";
 import { React, useState } from "react";
 import Timer from "./Timer";
+
 const Game = () => {
   const [inputCode, setInputCode] = useState("");
   const [question, setQuestion] = useState("Q1");
@@ -22,7 +23,6 @@ const Game = () => {
           'students = ["Jhn","John"]',
         ],
         [0, 2, 1],
-        3,
       ],
     ],
     [
@@ -31,7 +31,6 @@ const Game = () => {
         "Given two strings s and t, find out if they are equivalent and either return true if they are or false if they are not",
         ['s="Tree";t="tree"', 's="Love";t="Love"', 's="Test";t="Fail"'],
         [false, true, false],
-        7,
       ],
     ],
     [
@@ -41,7 +40,6 @@ const Game = () => {
       ],
       [""],
       [""],
-      10,
     ],
   ]);
 
@@ -63,9 +61,15 @@ for(let i = 0;i < students.length; i++) {
       eval(questions.get(question)[1][i]);
       eval(text);
       if (answer !== questions.get(question)[2][i]) {
-        console.log("Wrong!");
+        console.log("Code is Wrong!");
         return;
       }
+    }
+
+    if (question === "Q1") {
+      setQuestion("Q2");
+    } else if (question === "Q2") {
+      setQuestion("Q3");
     }
     console.log("Correct!");
   };
@@ -75,7 +79,7 @@ for(let i = 0;i < students.length; i++) {
   };
 
   const time = new Date();
-  const timerMinutes = questions.get(question)[3];
+  const timerMinutes = 15;
   time.setMinutes(time.getMinutes() + timerMinutes);
   return (
     <div>
@@ -97,7 +101,10 @@ for(let i = 0;i < students.length; i++) {
         cols="50"
         spellCheck="false"
       ></textarea>
-      <button onClick={() => evaluateFunction()}> Eval Function </button>
+      <button className="button" onClick={() => evaluateFunction()}>
+        {" "}
+        Sumbit{" "}
+      </button>
     </div>
   );
 };
