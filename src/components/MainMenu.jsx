9fd0d31b8fc44e362502d11238b955090fdc8346
Randy <randybrown9812@gmail.com>
2024-02-10 20:42:29 -0600

    Add Game

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
+import Nav from "../Nav";
 
 const MainMenu = () => {
-    return ( 
-        <div className="center">
-            <h1 className="space_font white_text"> Learning Project </h1>
-            <Link to="/game"> 
-                <button className="button space_font terminal_style"> Start Game </button>
-            </Link>
+    return (
+        <div>
+            <div className="space_font">
+                <Nav/>
+            </div>
+            <div className="center">
+                <h1 className="space_font white_text"> Learning Project </h1>
+                <Link to="/game"> 
+                    <button className="button space_font terminal_style"> Start Game </button>
+                </Link>
+            </div>
         </div>
     );
 }
